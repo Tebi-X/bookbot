7a534753292734777b448a51e6242256a5afe8e4
tebix <esteban.torres.perez@outlook.com>
2025-02-11 21:06:37 -0800

    main.py sorts every char in frankenstein book while counting each char

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-
+"""
 words = file_contents.split()
 
 word_counter = 0
@@ -10,3 +8,33 @@ for word in words:
         word_counter += 1
 
 print(word_counter)
+
+"""
+
+def sort_chars(file):
+    chars_dict = {}
+    lower_case_file = file.lower()
+    char_count = 0
+
+    for char in lower_case_file:
+        
+        if char in chars_dict:
+            char_count = chars_dict[char]
+            char_count += 1
+            chars_dict[char] = char_count
+        else:
+            chars_dict[char] = 1  
+
+    return chars_dict
+        
+def main():  
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+    sorted_dict = sort_chars(file_contents)
+
+    print(sorted_dict)
+
+import os
+os.system('clear')
+main()
